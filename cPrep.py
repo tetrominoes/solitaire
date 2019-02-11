5bace1fb0c4cec626cb42058854ec9a1df6da4f7
Trevor Wensman <twensman566@gmail.com>
2019-02-11 21:49:40 +0000

    Add Deck

---
+++ cPrep.py
@@ -0,0 +1,10 @@
+# tool for compiling C++ programs written in C++
+
+import os
+
+
+itemName = input('type name of file')
+
+os.system("g++ -o " + itemName + " " + itemName + ".cpp")
+print('compiled')
+os.system("./" + itemName)
