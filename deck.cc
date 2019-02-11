d665c54c74607ebbb6e1f790045e55ebdba070f4
jaime vega <vega.e.jaime@gmail.com>
2019-02-11 15:06:57 -0600

    added compiler script

---
+++ deck.cc
@@ -0,0 +1,30 @@
+//Deck implementation
+
+#include <iostream>
+#include <vector>
+#include <srdlib.h>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
+
+vector<card> shuffle(vector<card> deck){
+
+
+
+return deck
+}
+
+int generateDeck(){
+
+}
+int getDeck(){
+
+}
+int getNextCard(){
+
+}
