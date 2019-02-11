626587f1d1dee59b54725aadf149075ed3f9f260
jaime vega <vega.e.jaime@gmail.com>
2019-02-11 15:36:10 -0600

    first instance of yates algorithm incomplete

--- deck.cc
+++ deck.cc
@@ -13,14 +13,32 @@ int main(){
 }
 
 vector<card> shuffle(vector<card> deck){
-
+    int n = deck.size();
+    int a[n];
+    int ind[n];
+    for(int i = 0; i < n; i++){
+        do{
+            index  = rand() % n;
+        }while(ind[index] != 0);
+        ind[index] = 1;
+        a[1] = *(arr + index);
+    }
+    for(int k = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
 
 
 return deck
 }
 
 int generateDeck(){
-
+    vector<Card> temp;
+    for(int i = 0; i < 52; ++i){
+        Card singleCard;
+        singleCard.cardValue = ;//something
+        singleCard.color
+    }
+    
 }
 int getDeck(){
 
