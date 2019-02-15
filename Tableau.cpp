edb7b5695c9c87c023ef755aad7d6fbee28f9e3e
CHarvey820 <charvey820@gmail.com>
2019-02-15 04:25:48 +0000

    Foundation & Tableau

---
+++ Tableau.cpp
@@ -0,0 +1,35 @@
+//Class to represent a tableauPile.
+#include <iostream>
+#include "Pile.cpp"
+#include <vector>
+using namespace std;
+
+class TableauPile{
+
+   std::vector<Card> Cards;
+   int initSize;
+
+//TableauPile Method
+TableauPile(int value){
+
+}
+
+
+//bool canReceive: determines if the Tableau can recieve current card
+/*bool canReceieve{
+    //if Tableau is empty, and card is an ace
+    if (empty && (currentcard.getCardValue = 1)){
+        return true;
+    }
+    //current card is the same suit, and 1 more than the previous top card
+    else if(currentcard.getSuit() = topCard.getSuit() && currentcard.getValue() = topCard.getValue() + 1){
+        return true;
+    }
+    return false;
+}
+*/
+void placeCard(){
+
+}
+
+}
