ffb8f4b00a2242a8bb735a488f0021c947e7e2c9
jaime vega <vega.e.jaime@gmail.com>
2019-02-15 19:35:49 -0600

    updated driver

--- SolitaireDriver.cpp
+++ SolitaireDriver.cpp
@@ -7,6 +7,7 @@ int numGames = 0;
 vector<>
 //this vector will prob be a collection of vectors that
 //represent each 'pile'
+//also known as table for a single game
 int main(){
     cout << "Solitaire initialized";
     cout << "Enter number of games to play";
@@ -25,7 +26,12 @@ void newGame(){
 }
 //distribute cards for each of the games
 void setupTable(vector<Card> deck){
-  
+  //cycle thru all tableau's
+  for(int i = 0; i < 7; ++i){
+    for(){//cycle thru all individual tableaus
+      //add card to tableau
+  }
 
+    
 
 }
